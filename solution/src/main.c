8e326ec8f44799a43e14720d440867f1989e22dc
quanperfect <germandagil@gmail.com>
2022-01-10 09:08:20 +0300

    Increased readability

--- solution/src/main.c
+++ solution/src/main.c
@@ -5,6 +5,24 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAG   "\x1B[35m"
+#define RED   "\x1B[31m"
+#define GRN   "\x1B[32m"
+#define RESET "\x1B[0m"
+
+FILE* file;
+enum open_status open_status;
+enum close_status close_status;
+enum bmp_status bmp_status;
+struct image source_image = {0};
+struct image result_image = {0};
+
+void program_end_with_error() {
+	printf(RED "Program has ended with error code \n " RESET);
+	exit(1);
+}
 
 static const char* const bmp_error_messages[] = {
         "ERROR: THE REQUIRED FILE FORMAT IS BMP\n",
@@ -22,80 +40,88 @@ static const char* const close_error_messages[] = {
         "ERROR: UNKNOWN ERROR ¯\\_(ツ)_/¯\n"
 };
 
-bool open_status_check (enum open_status open_status) {
+bool open_status_check(enum open_status open_status) {
     if (open_status != OPEN_SUCCESS) {
-        fputs(open_error_messages[open_status], stderr);
-        return 1;
+				printf(RED "%s\n" RESET, open_error_messages[open_status]);
+        program_end_with_error();
     }
     return 0;
 }
 
-bool close_status_check (enum close_status close_status) {
+bool close_status_check(enum close_status close_status) {
     if (close_status != CLOSE_SUCCESS) {
-        fputs(close_error_messages[close_status], stderr);
-        return 1;
+				printf(RED "%s\n" RESET, close_error_messages[close_status]);
+        program_end_with_error();
     }
     return 0;
 }
 
-bool bmp_status_check (enum bmp_status bmp_status) {
+bool bmp_status_check(enum bmp_status bmp_status) {
     if (bmp_status != BMP_SUCCESS) {
-        fputs(bmp_error_messages[bmp_status], stderr);
-        return 1;
+				printf(RED "%s\n" RESET, bmp_error_messages[bmp_status]);
+        program_end_with_error();
     }
     return 0;
 }
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Manual for using this program:\n ./image-transformer \"source image\" \"transformed image\"\n");
-        return 1;
+void pre_check(int argc, char* argv[]) {
+	if (argc != 3) {
+        printf(RED "Manual for using this program:\n ./image-transformer source_image.bmp transformed_image.bmp\n" RESET);
+        program_end_with_error();
     }
 
-    FILE* file;
-    enum open_status open_status = file_open(&file, argv[1], "rb");
+    open_status = file_open(&file, argv[1], "rb");
 
     if (open_status_check(open_status)) {
-        return 1;
+        program_end_with_error();
     }
 
-    struct image source = {0};
-    enum bmp_status bmp_status = from_bmp(file, &source);
-    enum close_status close_status = file_close(&file);
+    bmp_status = from_bmp(file, &source_image);
+    close_status = file_close(&file);
 
     if (close_status_check(close_status)){
-        return 1;
+        program_end_with_error();
     }
 
     if (bmp_status_check(bmp_status)) {
-        destroy_image(&source);
-        return 1;
+        destroy_image(&source_image);
+        program_end_with_error();
     }
+}
 
-    struct image result = rotate(source);
-    destroy_image(&source);
-
-    open_status = file_open(&file, argv[2], "wb");
+void get_result_in_file(int argc, char* argv[]) {
+		
+		open_status = file_open(&file, argv[2], "wb");
     
     if (open_status_check(open_status)) {
-        destroy_image(&result);
-        return 1;
+        destroy_image(&result_image);
+        program_end_with_error();
     }
 
-    bmp_status = to_bmp(file, result);
+    bmp_status = to_bmp(file, result_image);
 
     close_status = file_close(&file);
 
     if (close_status_check(close_status)){
-        return 1;
+        program_end_with_error();
     }
 
-    destroy_image(&result);
+    destroy_image(&result_image);
 
     if (bmp_status_check(bmp_status)) {
-        return 1;
+        program_end_with_error();
     }
+}
 
-    fputs("The rotation was completed successfully.\nCheck the new created file with the name you assigned above.\n", stderr);
+int main(int argc, char* argv[]) {
+		printf(MAG "Image rotation lab by G. Dagil\n" RESET);
+
+		pre_check(argc, argv);
+ 
+    result_image = rotate(source_image);
+
+		get_result_in_file(argc, argv);
+
+		printf(GRN "The rotation was completed successfully." RESET "\nCheck the new created file with the name you assigned above.\n");
     return 0;
 }
